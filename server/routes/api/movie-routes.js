0d8a8301fafd33981dcbffde3e2dfa68c9f54fb8
Catherine Adegoke <cmadegoke@gmail.com>
2020-05-03 20:34:01 -0400

    server.js added

--- server/routes/api/movie-routes.js
+++ server/routes/api/movie-routes.js
@@ -2,7 +2,7 @@ const router = required('express').Router();
 const {getAllMovies,saveMovie,deleteMovie} = require ('../../controllers/movie-controller');
 
 //for GET and POST Movies /api/movies
-router.route('/').get(getAllMovies).post(saveBook);
+router.route('/').get(getAllMovies).post(saveMovie);
 
 
 //for delete /api/books/:id
