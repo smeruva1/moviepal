caa3b2ba24b5fdebc6a95d087d022ea55bc15a3d
Catherine Adegoke <cmadegoke@gmail.com>
2020-05-06 11:50:13 -0400

    clean

--- client/src/pages/SearchMovies.js
+++ client/src/pages/SearchMovies.js
@@ -1,16 +1,27 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext,useEffect } from 'react';
 import { Jumbotron, Container, Row, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import Navbar from '../components/Navbar';
+import queryString from 'query-string';
 
 import SavedMovieContext from '../utils/SavedMovieContext';
 
 import { saveMovie, searchOMDBMovies } from '../utils/API';
 
-function SearchMovies() {
+function SearchMovies(props) {
+    const { searchText } = queryString.parse(props.location.search)
+
     //create state for holding return OMDB api data
     const [searchedMovies, setSearchedMovies] = useState([]);
 
     //create state for holding our search field data
     const [searchInput, setSearchInput] = useState('');
+
+    useEffect(() => {
+        if (searchText) {
+            searchFor(searchText)
+        }
+    }, [])
+    
 
     //get saved movies from app.js on load
     const { movies: savedMovies, getSavedMovies } = useContext(SavedMovieContext);
@@ -22,18 +33,22 @@ function SearchMovies() {
         if (!searchInput) {
             return false;
         }
+        searchFor(searchInput)
+        
+    };
 
-        searchOMDBMovies(searchInput)
+    function searchFor(title) {
+        searchOMDBMovies(title)
             .then(({ data }) => {
                 // console.log(JSON.stringify (data));
                 let movieData = [];
                 if (data != null && data != null) {
                     // movieData.push(data.Search)
                     movieData = data.Search.map((movie) => ({
-                        movieId: movie.imdbID,
+                        id: movie.imdbID,
                         name: movie.Title,
-                        imageURL: movie.Poster || '',
-                        released: movie.Year
+                        ImageURL: movie.Poster,
+                        Released: movie.Year
                     }))
 
                 }
@@ -43,7 +58,7 @@ function SearchMovies() {
 
             .then(() => setSearchInput(''))
             .catch((err) => console.log(err));
-    };
+    }
 
     //create method to search for movies and set state on the form submit
     const handleSaveMovie = (movieId) => {
@@ -59,28 +74,6 @@ function SearchMovies() {
 
     return (
         <>
-            <Jumbotron fluid bg='dark' className="text-light bg-dark">
-                <Container>
-                    <h1> Search for Movies!</h1>
-                    <Form onSubmit={handleFormSubmit}>
-                        <Form.Row>
-                            <Col xs={12} md={8}>
-                                <Form.Control
-                                    name='searchInput'
-                                    value={searchInput}
-                                    onChange={(event) => setSearchInput(event.target.value)}
-                                    type='text'
-                                    size='lg'
-                                    placeholder='Search for a Movie'
-                                />
-                            </Col>
-                            <Col xs={12} md={4}>
-                                <Button type="submit"  variant='success' size='lg'>Submit Search</Button>
-                            </Col>
-                        </Form.Row>
-                    </Form>
-                </Container>
-            </Jumbotron>
             <Container fluid>
                 <h2>{searchedMovies.length ? `Viewing ${searchedMovies.length} results:` : 'search for a movie to begin'}</h2>
 
@@ -110,3 +103,5 @@ function SearchMovies() {
 }
 
 export default SearchMovies;
+
+
